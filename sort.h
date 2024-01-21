d496d7f7206a1fcf9f57ad1f6552061973c8ec8f
jospeh007 <youssefgf123@gmail.com>
2024-01-21 11:52:38 -0800

    Sort H file

---
+++ sort.h
@@ -0,0 +1,32 @@
+#ifndef _SORT_H
+#define _SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * struct listint_s - Element of a doubly linked list
+ *
+ * @n: Int sorted in the node
+ * @prev: The previous element of the list
+ * @next: The next element of the list
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* The printing functions */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* The sorting */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* SORT_H */
